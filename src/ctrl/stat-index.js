7dc2d028b105377ab6d989352b774d0ecab50e67
Seven Lju <dna2gle@gmail.com>
2024-05-23 23:38:00 +0800

    add dummy stat for all pages
    - stat-index
    - stat-search
    - stat-settings

---
+++ src/ctrl/stat-index.js
@@ -0,0 +1,6 @@
+const stat = {
+   // origin: { name: 'rsi' }
+   list: [],
+   // applied: { order: 0, name, type: 'rsi', params: [{ window: 6 }, { window: 12 }, { window: 20 }] }
+   view: []
+};
---
+++ src/ctrl/stat-search.js
@@ -0,0 +1,4 @@
+const stat = {
+   // { condition: { index: [{ <varname>: { ...applied-index } }] }, list: [{ code, name }] }
+   steps: []
+};
