8afff89eb9a6916115ae1651e3e04daac21bccd7
Seven Lju <dna2gle@gmail.com>
2024-05-23 23:39:14 +0800

    add dom utils class.contains (k_), attr related (a, ap, am)

--- src/ui/dom.js
+++ src/ui/dom.js
@@ -7,10 +7,14 @@ const api = {
    $p: function (elem, child) { elem.appendChild(child); },
    $m: function (elem, child) { elem.removeChild(child); },
    $c: function (elem) { while (elem.children.length) api.$m(elem, elem.children[0]); elem.textContent = ''; },
+   k_: function (elem, k) { return elem.classList.contains(k); },
    kp: function (elem, k) { elem.classList.add(k); },
    km: function (elem, k) { elem.classList.remove(k); },
    on: function (elem, name, fn) { elem.addEventListener(name, fn) },
    off: function (elem, name, fn) { elem.removeEventListener(name, fn) },
+   a: function (elem, key) { return elem.getAttribute(key); },
+   ap: function (elem, key, val) { elem.setAttribute(key, val); },
+   am: function (elem, key) { elem.removeAttribute(key); }
 };
 
 module.exports = api;
