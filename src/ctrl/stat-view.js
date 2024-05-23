2a1ea814fd1cffef04b24747833614f71d6fc29c
Seven Lju <dna2gle@gmail.com>
2024-05-23 23:34:22 +0800

    add stat-view to record view stat

---
+++ src/ctrl/stat-view.js
@@ -0,0 +1,28 @@
+const stat = {
+   // - read list from db, filtered as list
+   // - click one in the list displayed, read one data from db, populated to data
+   // - read index settings, calc index
+   // - draw data and index in canvas
+   list: [],
+listx: [
+   { fav: true, code: 'sh000000', name: 'test', latest: { O: 23.23, C: 34.13, H: 35.3, L: 22.5, V: 23459, m: 1111 } },
+   { fav: false, code: 'sh000001', name: 'test2', latest: { O: 53.23, C: 34.13, H: 55.3, L: 22.5, V: 323459, m: 1111 } },
+],
+   data: [],
+   // { name: 'group', type: 'rsi', params: [{ window: 6 }, ...], data: [[...], ...] }
+   index: [],
+   // fetch data from internet
+   network: {},
+   filter: {
+      bj: true,
+      sh: true,
+      sz: true,
+      primary: true,
+      fav: false,
+   },
+   prevUri: null,
+   uri: '',
+   dirty: true,
+};
+
+module.exports = stat;
