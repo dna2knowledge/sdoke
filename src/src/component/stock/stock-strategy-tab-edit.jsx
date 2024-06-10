582800688a3cbbc904b6d1227aa32f8f0053afda
Seven Lju <dna2gle@gmail.com>
2024-06-10 16:15:38 +0800

    init stock strategy one edit tab

--- src/src/component/stock/stock-strategy-tab-edit.jsx
+++ src/src/component/stock/stock-strategy-tab-edit.jsx
@@ -1,6 +1,67 @@
-import { Box } from '@mui/material';
+import { useState } from 'react';
+import { Box, TextField, Grid, Button, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+
+function StrategyVariable(props) {
+   const { data } = props;
+   const [variables, setVariables] = useState(data || []);
+   return <Box sx={{ mt: 1, mb: 1 }}>
+      Variables
+      <Box sx={{ color: '#999', fontSize: '10px' }}>Define variables for using in score rules</Box>
+      {variables.map(z => <Grid container spacing={2}>
+         <Grid item xs={4}><TextField fullWidth label="Name" variant="standard" /></Grid>
+         <Grid item xs={8}><TextField fullWidth label="Formula" variant="standard" /></Grid>
+      </Grid>)}
+      <Box><IconButton><AddIcon /></IconButton></Box>
+   </Box>;
+}
+
+function StrategyScoreRule(props) {
+   const { data } = props;
+   const [rules, setRules] = useState(data || []);
+   return <Box sx={{ mt: 1, mb: 1 }}>
+      Score Rules
+      <Box sx={{ color: '#999', fontSize: '10px' }}>Define rules for buy/sell scoring; 1 is the strongest buying signal, meanwhile -1 is the strongest selling signal</Box>
+      {rules.map(z => <Grid container spacing={2}>
+         <Grid item xs={6}><TextField fullWidth label="Condition" variant="standard" /></Grid>
+         <Grid item xs={6}><TextField fullWidth label="Score Formula" variant="standard" /></Grid>
+      </Grid>)}
+      <Box><IconButton><AddIcon /></IconButton></Box>
+   </Box>;
+}
+
+function StrategyVisualization(props) {
+   const { data } = props;
+   const [visualizations, setVisualizations] = useState(data || []);
+   return <Box sx={{ mt: 1, mb: 1 }}>
+      Visualization
+      <Box sx={{ color: '#999', fontSize: '10px' }}>Define groups for visualization</Box>
+      {visualizations.map(z => <Grid container spacing={2}>
+         <Grid item xs={4}><TextField fullWidth label="Name" variant="standard" /></Grid>
+         <Grid item xs={8}><TextField fullWidth label="Formula" variant="standard" /></Grid>
+      </Grid>)}
+      <Box><IconButton><AddIcon /></IconButton></Box>
+   </Box>;
+}
 
 export default function StockStrategyEditTab (props) {
-   const { tab } = props;
-   return <Box sx={{ display: tab === 'edit' ? 'flex' : 'none' }}>Strategy Overview/Edit</Box>;
+   const { tab, data } = props;
+   const [name, setName] = useState(data?.name || '');
+   const [desc, setDesc] = useState(data?.desc || '');
+
+   return <Box sx={{ display: tab === 'edit' ? 'block' : 'none', overflowY: 'auto', height: '0px', flex: '1 0 auto' }}>
+      <Box>
+         <Button>Save</Button>
+         <Button>Cancel/Reset</Button>
+         <Button>Fill Example</Button>
+      </Box>
+      <Box>
+         Basic Info
+         <TextField fullWidth label="Strategy Name" variant="standard" />
+         <TextField fullWidth label="Strategy Description" variant="standard" />
+      </Box>
+      <StrategyVariable data={data?.var} />
+      <StrategyScoreRule data={data?.rule} />
+      <StrategyVisualization data={data?.vis} />
+   </Box>;
 }
--- src/src/component/stock/stock-strategy.jsx
+++ src/src/component/stock/stock-strategy.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Box, Tabs, Tab, Autocomplete, TextField, Button } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import NoData from '$/component/shared/no-data';
@@ -6,10 +6,47 @@ import StockStrategyEditTab from '$/component/stock/stock-strategy-tab-edit';
 import StockStrategySuggestTab from '$/component/stock/stock-strategy-tab-suggest';
 import eventbus from '$/service/eventbus';
 import databox from '$/service/databox';
+import local from '$/service/local';
 
 export default function StockStrategy() {
    const [tab, setTab] = useState('edit');
    const [strategyList, setStrategyList] = useState([]);
+   const [selected, setSelected] = useState(local.data.selectedStockStrategy);
+
+   useEffect(() => {
+      eventbus.on('stock.strategy.add', onStockStrategyAdd);
+      eventbus.on('stock.strategy.edit', onStockStrategyEdit);
+      return () => {
+         eventbus.off('stock.strategy.add', onStockStrategyAdd);
+         eventbus.off('stock.strategy.edit', onStockStrategyEdit);
+      };
+
+      function onStockStrategyAdd() {
+         if (local.data.selectedStockStrategy && local.data.selectedStockStrategy.dirty) {
+            if (!confirm("The previous strategy has been updated but not saved; do you want to discard the data and create new one?")) return;
+         }
+         setTab('edit');
+         const item = {
+            name: '',
+            desc: '',
+            new: true,
+            dirty: true,
+            var: [], rule: [], vis: [],
+         };
+         local.data.selectedStockStrategy = item;
+         setSelected(item);
+      }
+      function onStockStrategyEdit(item) {
+         if (item && local.data.selectedStockStrategy && local.data.selectedStockStrategy.dirty) {
+            if (!confirm("The previous strategy has been updated but not saved; do you want to discard the data and create new one?")) return;
+         }
+         setTab('edit');
+         local.data.selectedStockStrategy = item;
+         setSelected(item);
+      }
+   }, []);
+
+   const onCreateStrategyClick = () => eventbus.emit('stock.strategy.add');
 
    return <Box sx={{ width: '100%', height: '100%', maxWidth: '800px', minWidth: '200px', margin: '0 auto', display: 'flex', flexDirection: 'column' }}>
       <Box sx={{ mb: '10px' }}>
@@ -17,22 +54,27 @@ export default function StockStrategy() {
             options={strategyList}
             getOptionLabel={option => `${option.name}`}
             onChange={(_, val) => {
-               eventbus.emit('stock.pinned.add', val);
+               eventbus.emit('stock.strategy.edit', val);
             }}
+            renderOption={(props, item) => <li key={item.name} {...props}><StockStrategyOption data={item} /></li>}
             renderInput={params =>
                <TextField sx={{ width: '100%', borderBottom: '1px solid #ccc' }} placeholder="Strategy Name"
                   {...params}
                />
             }
             noOptionsText={<Box>
-               No available strategy. <Button sx={{ marginLeft: '5px' }} variant="contained" startIcon={<EditIcon />}>
-                  Create Strategy
+               No available strategy. <Button
+                  sx={{ marginLeft: '5px' }}
+                  variant="contained"
+                  startIcon={<EditIcon />}
+                  onClick={onCreateStrategyClick}>
+                     Create Strategy
                </Button>
             </Box>}
          />
       </Box>
-      {strategyList.length ?
-      (<Box><Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      {selected ?
+      (<Box sx={{ display: 'flex', flexDirection: 'column', flex: '1 0 auto' }}><Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={tab} onChange={(_, val) => setTab(val)}>
           <Tab value="edit" label="Basic Info" />
           <Tab value="suggest" label="Search" />
@@ -40,8 +82,29 @@ export default function StockStrategy() {
       </Box>
       <StockStrategyEditTab tab={tab} />
       <StockStrategySuggestTab tab={tab} /></Box>) :
-      <NoData>No Strategy data; please search and select one or <Button sx={{ marginLeft: '5px' }} variant="contained" startIcon={<EditIcon />}>
-         Create Strategy
+      <NoData>No Strategy data; please search and select one or <Button
+         sx={{ marginLeft: '5px' }}
+         variant="contained"
+         startIcon={<EditIcon />}
+         onClick={onCreateStrategyClick}>
+            Create Strategy
       </Button></NoData>}
+   </Box>
+}
+
+function StockStrategyOption(props) {
+   const { data } = props;
+   return <Box sx={{ width: '100%' }}>
+      <Box><strong>{data.name}</strong></Box>
+      {data.desc ? <Box sx={{
+         fontSize: '0.8rem',
+         color: 'gray',
+         width: '100%',
+         overflow: 'hidden',
+         textOverflow: 'ellipsis',
+         whiteSpace: 'nowrap'
+      }}>
+         {data.desc}
+      </Box> : null}
    </Box>
 }
